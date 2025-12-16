d283df4ba8aef98162db3d157dd60e606ad590d5
Sanyukta Kale <sakshikale026@gmail.com>
2025-12-16 09:07:13 +0530

    add assignment2-Q2

---
+++ day2/main.py
@@ -0,0 +1,9 @@
+import cal
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", cal.add(a, b))
+print("Subtraction:", cal.subtract(a, b))
+print("Multiplication:", cal.multiply(a, b))
+print("Division:", cal.divide(a, b))
